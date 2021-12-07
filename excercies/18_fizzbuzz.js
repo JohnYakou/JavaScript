31cb18f8f0dd905a08b282c3b3b18957f6266656
YakouJohn <jyakou@gmx.fr>
2021-12-07 16:44:34 +0100

    correction

--- excercies/18_fizzbuzz.js
+++ excercies/18_fizzbuzz.js
@@ -9,3 +9,12 @@ for (var i = 1; i <= 100; i++) {
         console.log(i)
     }
 }
+
+/* CORRECTION
+
+for (var nombre = 1; nombre <= 100; nombre++){
+    if((nombre % 3 === 0) && (nombre % 5 === 0)) console.log("FizzBuzz");
+    else if(nombre % 3 === 0) console.log("Fizz");
+    else if(nombre % 5 === 0) console.log("Buzz");
+    else console.log(nombre);
+} */
